ad91c66e824c459d005f5b7df55d44a18f811955
Wagner Xu <wagnerhsu@hotmail.com>
2021-02-18 21:32:06 +0800

    Add files

--- Chapter05and06/Chapter_05_and_06_Blazor_Examples/TouristSpot/BlazorServer.Web/Data/Place.cs
+++ Chapter05and06/Chapter_05_and_06_Blazor_Examples/TouristSpot/BlazorServer.Web/Data/Place.cs
@@ -6,12 +6,18 @@ namespace BlazorServer.Web.Data
     public class Place
     {
         public int Id { get; set; }
-        [Required] public string Name { get; set; }
-        [Required] public string Location { get; set; }
-        [Required] public string About { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string About { get; set; }
+
         public int Reviews { get; set; }
         public string ImageData { get; set; }
         public DateTime LastUpdated { get; set; }
     }
 }
-
